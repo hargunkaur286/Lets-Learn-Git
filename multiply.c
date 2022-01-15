3fe0e3cf838e4fe3a08913a3a776659b9b9ee218
Hargun Kaur <hargunkaur286@gmail.com>
2022-01-16 00:25:56 +0530

    added multiply.c

---
+++ multiply.c
@@ -0,0 +1,12 @@
+int main()
+{
+    int number1, number2, product;
+    printf(" Enter two integers: ");
+    scanf("%d %d", &number1, &number2);
+    product = number1 * number2;
+    printf("%d * %d = %d", number1, number2, product);
+
+
+    return 0;
+
+}
