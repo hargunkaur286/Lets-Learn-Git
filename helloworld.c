61c4d075d221838ef2faad2931e2f8b6602165ae
Hargun Kaur <hargunkaur286@gmail.com>
2022-01-16 00:15:55 +0530

    added message in C

--- helloworld.c
+++ helloworld.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 int main(){
-    printf("\nHello and welcome to the world of C");
+    printf("\nHello and welcome to the world of C and this is the modification");
     return 0;
-    
+
 
 }
 
